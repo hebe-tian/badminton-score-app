097cde9766b9827830fa830ba9c86feb30d73a0a
hebe_tien <linleil@126.com>
2026-04-27 18:09:20 +0800

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   // GitHub Pages 部署时需要设置 base 路径
   // 如果部署到 https://username.github.io/ ，使用 '/'
   // 如果部署到 https://username.github.io/repo-name/ ，使用 '/repo-name/'
-  base: 'https://username.github.io/badminton-score-app/',
+  base: '/badminton-score-app/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
